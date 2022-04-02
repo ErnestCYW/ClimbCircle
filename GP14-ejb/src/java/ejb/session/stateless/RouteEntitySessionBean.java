0a4b968abdbc0633be78c39c36fcf3a85cd55328
jamesyakjy <jamesyakjy@gmail.com>
2022-04-03 01:04:07 +0800

    Added Route Management - Route Entity Session Bean

---
+++ GP14-ejb/src/java/ejb/session/stateless/RouteEntitySessionBean.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateless;
+
+import entity.RouteEntity;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author jamesyak
+ */
+@Stateless
+public class RouteEntitySessionBean implements RouteEntitySessionBeanLocal {
+
+    @PersistenceContext(unitName = "GP14-ejbPU")
+    private EntityManager em;
+
+    public void persist(Object object) {
+        em.persist(object);
+    }
+
+    @Override
+    public Long createNewRoute(RouteEntity newRouteEntity) {
+        em.persist(newRouteEntity);
+        em.flush();
+
+        return newRouteEntity.getRouteId();
+    }
+
+    @Override
+    public RouteEntity retrieveRouteByRouteId(Long routeId) {
+        Query query = em.createQuery("SELECT r FROM RouteEntity r WHERE r.routeID =: routeID");
+        query.setParameter("routeId", routeId);
+
+        return (RouteEntity) query.getSingleResult();
+    }
+
+    @Override
+    public RouteEntity retrieveRouteByRouteName(String routeName) {
+        Query query = em.createQuery("SELECT r FROM RouteEntity r WHERE r.routeName =: routeName");
+        query.setParameter("routeName", routeName);
+        return (RouteEntity) query.getSingleResult();
+    }
+
+    @Override
+    public List<RouteEntity> retrieveAllRoutes() {
+        Query query = em.createQuery("SELECT r FROM RouteEntity r");
+        return query.getResultList();
+    }
+
+    @Override
+    public void deleteRoute(Long routeId) {
+        RouteEntity routeEntityToRemove = retrieveRouteByRouteId(routeId);
+        em.remove(routeEntityToRemove);
+    }
+}
