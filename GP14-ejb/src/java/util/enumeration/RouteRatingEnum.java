caab8ee26b5e3a9d206be3ffd2363a87f7670f23
jamesyakjy <jamesyakjy@gmail.com>
2022-04-03 01:03:18 +0800

    New Enum - Route Rating

---
+++ GP14-ejb/src/java/util/enumeration/RouteRatingEnum.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util.enumeration;
+
+/**
+ *
+ * @author jamesyak
+ */
+public enum RouteRatingEnum {
+    VB,
+    V0to1,
+    V1to2,
+    V2to4,
+    V4to6,
+    V6to8,
+    V8
+}
