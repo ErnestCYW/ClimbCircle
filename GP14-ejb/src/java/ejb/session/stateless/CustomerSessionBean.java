e0c4d05ec2eec51dd78e1867d664f554d16c60e2
GingermanA <elginchan13@gmail.com>
2022-04-10 18:47:06 +0800

    Added RWS for Customer login on Ionic

---
+++ GP14-ejb/src/java/ejb/session/stateless/CustomerSessionBean.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateless;
+
+import entity.Admin;
+import entity.Customer;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import util.exception.AdminNotFoundException;
+import util.exception.CustomerNotFoundException;
+import util.exception.InvalidLoginCredentialException;
+
+/**
+ *
+ * @author elgin
+ */
+@Stateless
+public class CustomerSessionBean implements CustomerSessionBeanLocal {
+
+    @PersistenceContext(unitName = "GP14-ejbPU")
+    private EntityManager em;
+
+    @Override
+    public Long createNewCustomer(Customer newCustomer) {
+        em.persist(newCustomer);
+        em.flush();
+        
+        return newCustomer.getCustomerId();
+    }
+    
+    
+    @Override
+    public Customer retrieveCustomerByUsername(String username) throws CustomerNotFoundException {
+        Query query = em.createQuery("SELECT c from Customer c WHERE c.username = :username");
+        query.setParameter("username", username);
+        
+        try {
+            return (Customer)query.getSingleResult();
+        } catch (NoResultException ex) {
+            throw new CustomerNotFoundException("Customer does not exist");
+        }
+        
+    }
+    
+    @Override
+    public Customer login(String username, String password) throws InvalidLoginCredentialException {
+
+        try {
+            Customer customer = retrieveCustomerByUsername(username);
+            if (customer.getPassword().equals(password)) {
+                return customer;
+            } else {
+                throw new InvalidLoginCredentialException("Username does not exist or invalid password");
+            }
+        
+        } catch (CustomerNotFoundException ex) {
+            throw new InvalidLoginCredentialException("Username does not exist or invalid password");
+        }
+        
+        
+    }
+
+    
+}
---
+++ GP14-rws/src/java/ws/rest/CustomerResource.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.rest;
+
+import ejb.session.stateless.CustomerSessionBeanLocal;
+import entity.Customer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+/**
+ * REST Web Service
+ *
+ * @author elgin
+ */
+@Path("Customer")
+public class CustomerResource {
+
+    CustomerSessionBeanLocal customerSessionBean = lookupCustomerSessionBeanLocal();
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of CustomerResource
+     */
+    public CustomerResource() {
+    }
+
+    @Path("login")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response login(@QueryParam("username") String username, @QueryParam("password") String password) {
+        try {
+            Customer customer = customerSessionBean.login(username, password);
+
+            GenericEntity<Customer> genericEntity = new GenericEntity<Customer>(customer) {
+            };
+
+            return Response.status(Status.OK).entity(genericEntity).build();
+        } catch (Exception ex) {
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+        }
+    }
+
+    private CustomerSessionBeanLocal lookupCustomerSessionBeanLocal() {
+        try {
+            javax.naming.Context c = new InitialContext();
+            return (CustomerSessionBeanLocal) c.lookup("java:global/GP14/GP14-ejb/CustomerSessionBean!ejb.session.stateless.CustomerSessionBeanLocal");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+
+}
